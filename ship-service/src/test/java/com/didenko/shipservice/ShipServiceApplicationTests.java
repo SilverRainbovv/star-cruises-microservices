e63fe78c52a9cbf2646486a722ac3a8c1a21ed19
SilverRainbovv <vadimdidenko156@gmail.com>
2024-06-25 12:48:51 +0300

    init ship-service

---
+++ ship-service/src/test/java/com/didenko/shipservice/ShipServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.didenko.shipservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShipServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
