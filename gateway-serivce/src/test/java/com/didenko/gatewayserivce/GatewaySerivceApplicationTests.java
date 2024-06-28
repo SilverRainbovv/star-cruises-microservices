a5ef273039200fbb1287da6590df78a23bf9c6c1
SilverRainbovv <vadimdidenko156@gmail.com>
2024-06-28 20:54:23 +0300

    init gateway service

---
+++ gateway-serivce/src/test/java/com/didenko/gatewayserivce/GatewaySerivceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.didenko.gatewayserivce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewaySerivceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
